fb5ef4477fae13d58a139e1d555ff6cf84546286
Atharva-arvind-Bhalbar <91202125+Atharva-arvind-Bhalbar@users.noreply.github.com>
2021-12-29 15:09:34 +0530

    Add files via upload

---
+++ kwitermain.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAAddz5cwK1zbhICTLWcKfTbSSNle723uo",
+    authDomain: "kwitter--o.firebaseapp.com",
+    projectId: "kwitter--o",
+    storageBucket: "kwitter--o.appspot.com",
+    messagingSenderId: "254296247089",
+    appId: "1:254296247089:web:7268d52553aae58735be10"
+  };
+
+const app = initializeApp(firebaseConfig);
